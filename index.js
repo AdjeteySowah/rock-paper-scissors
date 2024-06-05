9cf00aa58bc032ff0a3e8396cc7951490113dd77
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-06-05 09:52:25 +0000

    Rewrite the logic for playing the game and displaying scores

--- index.js
+++ index.js
@@ -1,58 +1,85 @@
-    
-    // Define a function to request for a player's choice between "rock", "paper", or "scissors"
-function getHumanChoice() {
-        // Store the player's choice in a variable and return it ensuring that it's case insensitive.
-    let humanChoice = prompt("Rock Paper Scissors ?");
-    let insensitiveHumanChoice = `${humanChoice.slice(0, 1).toUpperCase()}${humanChoice.slice(1, humanChoice.length).toLowerCase()}`;
 
-    if (insensitiveHumanChoice !== "Rock" && insensitiveHumanChoice !== "Paper" && insensitiveHumanChoice !== "Scissors") {
-        insensitiveHumanChoice = prompt("Invalid choice. Please enter rock, paper, or scissors.");
-        insensitiveHumanChoice = `${insensitiveHumanChoice.slice(0, 1).toUpperCase()}${insensitiveHumanChoice.slice(1, insensitiveHumanChoice.length).toLowerCase()}`;
+window.onload = function() {
+    if (!localStorage.getItem('alertsShown')) {
+        alert(`Hi! Thanks for checking out my web app. This game follows the usual rock-paper-scissors rules. If you already know how to play, you can click "OK" to start playing. If not, please read the next pop-up for instructions.
+        `);
+
+        alert(`1. Rock crushes scissors.\n2. Paper covers rock.\n3. Scissors cuts paper.\n\nFor example, if you choose rock and the computer chooses scissors, then you win.
+        `);
+        localStorage.setItem('alertsShown', 'true');
     }
-    
-    return insensitiveHumanChoice;
 }
 
-    // Define a function to randomly  select one of 3 strings: "rock", "paper", or "scissors"
+let svgs = document.querySelectorAll(".button-svg");
+svgs.forEach((svg) => {
+    svg.addEventListener("click", playRound);
+});
+
+
 function getComputerChoice() {
-        // Declare an array to store the three possible choices.
-    let computerChoices = ["Rock", "Paper", "Scissors"];
-        // Generate a random index within the range of the array length.
+    let computerChoices = ["ROCK", "PAPER", "SCISSORS"];
     let computerChoiceIndex = Math.floor(Math.random() * computerChoices.length);
-        // Use the random index to select and output the corresponding string.
     return computerChoices[computerChoiceIndex];
 }
 
-    // Initialize variables to store the player's and computer's score before the start of the game
-let humanScore = 0;
-let computerScore = 0;
+function playRound(event) {
+    let grandestParent = event.target.closest(".button-svg");
+    let humanChoice = grandestParent.id;
+    let computerChoice = getComputerChoice();
 
-function playRound() {
-        // Play a series of rounds of the game
-    for (let round = 1; round <= 5; round++) {
-            // Get the player's and computer's choices for this round
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-            // Display the player's and computer's choices
-        console.log(humanSelection);
-        console.log(computerSelection);
-             // Determine the winner of this round and update scores accordingly
-        if (humanSelection === computerSelection) {
-            console.log("You win! You read the computer's mind 👏✌️.");
-            humanScore++;
-        } else {
-            console.log(`You lose 👎! ${computerSelection} beats ${humanSelection}.`);
-            computerScore++;
-        }
-    }
+    let yourAddedText = document.querySelector(".your-added-text");
+    yourAddedText.textContent = humanChoice;
+    let computerAddedText = document.querySelector(".computer-added-text");
+    computerAddedText.textContent = computerChoice;
 
-        // Create an object to display the final scores of the game in a tabular form
-    let displayScore = {
-        player: humanScore,
-        compter: computerScore,
-      };
-
-    console.table(displayScore);
+    declareWinner(humanChoice, computerChoice);
 }
 
-playRound();
+function declareWinner(humanChoice, computerChoice) {
+    let declaration = document.querySelector(".declaration");
+
+    if (humanChoice === computerChoice) {
+        declaration.textContent = "IT'S A TIE";
+        declaration.style.cssText = `font-size: 1.5rem;
+                                     font-weight: 600;
+                                     display: inline-block; 
+                                     background-color: rgba(128, 128, 128, 0.1);
+                                     color: rgb(128, 128, 128);
+                                     padding: 0.5rem 3rem`;
+    } else if (
+        (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+        (humanChoice === "PAPER" && computerChoice === "ROCK") ||
+        (humanChoice === "SCISSORS" && computerChoice === "PAPER") 
+    )   {
+        declaration.textContent = "YOU WIN";
+        declaration.style.cssText = `font-size: 1.5rem;
+                                     font-weight: 600;
+                                     display: inline-block;
+                                     background-color: rgba(0, 189, 145, 0.1);
+                                     color: rgb(0, 189, 145);
+                                     padding: 0.5rem 3rem`;
+    } else {
+        declaration.textContent = "YOU LOSE";
+        declaration.style.cssText = `font-size: 1.5rem;
+                                     font-weight: 600;
+                                     display: inline-block;
+                                     background-color: rgba(255, 87, 87, 0.1);
+                                     color: rgb(255, 87, 87);
+                                     padding: 0.5rem 3rem`;
+    }
+
+    awardScores(declaration);
+}
+
+function awardScores(declaration) {
+    let playerScore = document.querySelector(".player-score");
+    let initialPlayerScore = parseInt(playerScore.textContent, 10);
+    let computerScore = document.querySelector(".computer-score");
+    let initialComputerScore = parseInt(computerScore.textContent, 10);
+
+    if (declaration.textContent === "YOU WIN") {
+        playerScore.textContent = initialPlayerScore + 1;
+    } else if (declaration.textContent === "YOU LOSE") {
+        computerScore.textContent = initialComputerScore + 1;
+    }
+}
